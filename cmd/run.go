ebd17fc20bff3f555570bbcc887e8fce6d20b684
Aki Ali-Vehmas <akiali-vehmas@Akis-MacBook-Pro-2.local>
2020-10-25 10:48:36 +0200

    changed help text

--- cmd/run.go
+++ cmd/run.go
@@ -26,13 +26,10 @@ import (
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Train the som based on data",
+	Long: `Train the self-organizing map based on data (csv)
+	Self-organizing map maps the input data into two-dimensional
+	topology-preserving space by using competitive learning`,
 	Run: func(cmd *cobra.Command, args []string) {
 		x, _ := cmd.Flags().GetInt("x")
 		y, _ := cmd.Flags().GetInt("y")
